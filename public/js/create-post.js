ba7cb1057d7bfd5abbd7ac4a21e777a03d26f412
Huaquin Anchondo <hanchondo21@gmail.com>
2023-09-18 00:47:21 -0700

    create new post implemented

--- public/js/create-post.js
+++ public/js/create-post.js
@@ -1,1 +1,27 @@
-async function createPostFormHandler(event) {}
+async function createPostFormHandler(event) {
+  event.preventDefault();
+
+  const title = document.querySelector('#create-post-title').value.trim();
+  const content = document.querySelector('#create-post-content').value.trim();
+
+  if (title && content) {
+    const response = await fetch('/api/post', {
+      method: 'POST',
+      body: JSON.stringify({
+        title,
+        content
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+
+    if(response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert(response.statusText);
+    }
+  }
+}
+
+document.querySelector('.create-post-form').addEventListener('submit', createPostFormHandler);

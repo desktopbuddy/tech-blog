9c2f59cfc095f3dfe48f8be45f87ba270dd6bd55
Huaquin Anchondo <hanchondo21@gmail.com>
2023-09-18 08:45:19 -0700

    Implement login

--- models/User.js
+++ models/User.js
@@ -1,7 +1,13 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
 
-class User extends Model {}
+class User extends Model {
+  // check password function
+  checkPassword(pwLogin) {
+    return bcrypt.compareSync(pwLogin, this.password);
+  }
+}
 
 User.init(
     {
@@ -24,6 +30,16 @@ User.init(
       },
     },
     {
+      hooks: {
+        async beforeCreate(newUserData) {
+          newUserData.password = await bcrypt.hash(newUserData.password, 10);
+          return newUserData;
+        },
+        async beforeUpdate(updatedUserData) {
+          updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+          return updatedUserData;
+        }
+      },
       sequelize,
       timestamps: false,
       freezeTableName: true,
--- public/js/login.js
+++ public/js/login.js
@@ -1,11 +1,13 @@
-const loginFormHandler = async (event) => {
+async function loginFormHandler(event) {
   event.preventDefault();
+
+  console.log('handling login form');
 
   const username = document.querySelector('#username-login').value.trim();
   const password = document.querySelector('#password-login').value.trim();
 
   if (username && password) {
-    const res = await fetch('/api/users/login', {
+    const response = await fetch('/api/users/login', {
       method: 'POST',
       body: JSON.stringify({ username, password }),
       headers: { 'Content-Type': 'application/json' },

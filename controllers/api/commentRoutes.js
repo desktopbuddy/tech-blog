17996b3f1cd2d0445ad398d1a36ecf38b2d7be39
Huaquin Anchondo <hanchondo21@gmail.com>
2023-09-17 22:59:02 -0700

    style comment

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { Comment } = require('../../models');
+const withAuth = require('../../utils/auth');
 
 // post a new comment
 router.post('/', withAuth, async(req, res) => {

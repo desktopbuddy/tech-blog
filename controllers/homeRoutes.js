0034a46a088eb2a307e10183b57a91baaef49649
Huaquin Anchondo <hanchondo21@gmail.com>
2023-09-17 20:05:43 -0700

    update homeroute with loggedin variable

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -22,6 +22,7 @@ router.get('/', async (req, res) => {
 
     res.render('homepage', {
       posts,
+      logged_in: req.session.logged_in,
     });
   } catch (err) {
     console.log(err);
@@ -31,6 +32,10 @@ router.get('/', async (req, res) => {
 
 
 router.get('/login', (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect('/');
+    return;
+  }
   res.render('login');
 });
 

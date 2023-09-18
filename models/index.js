d381cdebe6645be001dc26e4950d5d891d60498f
Huaquin Anchondo <hanchondo21@gmail.com>
2023-09-17 18:10:53 -0700

    create model associations

--- models/index.js
+++ models/index.js
@@ -2,4 +2,31 @@ const User = require('./User');
 const Post = require('./Post');
 const Comment = require('./Comment');
 
+//one user has many posts, each post belongs to one user
+User.hasMany(Post, {
+  foreignKey: 'creator_id',
+});
+
+Post.belongsTo(User, {
+  foreignKey: 'creator_id',
+});
+
+// one blogpost has many comments, each comment belongs to one post
+Post.hasMany(Comment, {
+  foreignKey: 'post_id',
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id',
+});
+
+// one user has many comments, each comment belongs to one user
+User.hasMany(Comment, {
+  foreignKey: 'creator_id',
+});
+
+Comment.belongsTo(User, {
+  foreignKey: 'creator_id',
+})
+
 module.exports = { User, Post, Comment };
